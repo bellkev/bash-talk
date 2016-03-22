31bd73652b182082c11070584fdb3cca773bbbdc
Kevin Bell <kevin.a.bell@gmail.com>
2016-03-22 16:40:58 -0700

    Broken function

--- search.sh
+++ search.sh
@@ -1,15 +1,23 @@
 #! /bin/bash
 
 
-old_hotness=""
+hottest() {
+    url="https://api.github.com/search/repositories"
+    query_params="o=desc&q=language:$1&sort=updated&per_page=1"
+    return "$(curl -s "$url?$query_params" | jq -r '.items | .[] | .name')"
+}
 
-while true; do
-    new_hotness="$(curl -s "https://api.github.com/search/repositories?o=desc&q=language:JavaScript&sort=updated&per_page=1" | jq -r '.items | .[] | .name')"
-    if [[ $old_hotness != $new_hotness ]]; then
-        message="Alert! The hottest JavaScript project is $new_hotness"
-        echo "$message"
-        say "$message"
-        old_hotness="$new_hotness"
-    fi
-    sleep 1
-done
+hottest JavaScript
+
+# old_hotness=""
+
+# while true; do
+#     new_hotness=
+#     if [[ $old_hotness != $new_hotness ]]; then
+#         message="Alert! The hottest JavaScript project is $new_hotness"
+#         echo "$message"
+#         say "$message"
+#         old_hotness="$new_hotness"
+#     fi
+#     sleep 1
+# done
