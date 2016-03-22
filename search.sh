be4230b34f0ab7f21420da4bbe054f39528edcdd
Kevin Bell <kevin.a.bell@gmail.com>
2016-03-22 16:32:41 -0700

    Loop and speak

--- search.sh
+++ search.sh
@@ -1,3 +1,15 @@
 #! /bin/bash
 
-curl -s "https://api.github.com/search/repositories?o=desc&q=language:JavaScript&sort=updated" | jq -r '.items | .[] | .name'
+
+old_hotness=""
+
+while true; do
+    new_hotness="$(curl -s "https://api.github.com/search/repositories?o=desc&q=language:JavaScript&sort=updated&per_page=1" | jq -r '.items | .[] | .name')"
+    if [[ $old_hotness != $new_hotness ]]; then
+        message="Alert! The hottest JavaScript project is $new_hotness"
+        echo "$message"
+        say "$message"
+        old_hotness="$new_hotness"
+    fi
+    sleep 1
+done
