083ed28fca9657e85064412eeb20c22ee1c00571
Kevin Bell <kevin.a.bell@gmail.com>
2016-03-22 16:43:06 -0700

    Working function

--- search.sh
+++ search.sh
@@ -4,20 +4,19 @@
 hottest() {
     url="https://api.github.com/search/repositories"
     query_params="o=desc&q=language:$1&sort=updated&per_page=1"
-    return "$(curl -s "$url?$query_params" | jq -r '.items | .[] | .name')"
+    curl -s "$url?$query_params" | jq -r '.items | .[] | .name'
 }
 
-hottest JavaScript
 
-# old_hotness=""
+old_hotness=""
 
-# while true; do
-#     new_hotness=
-#     if [[ $old_hotness != $new_hotness ]]; then
-#         message="Alert! The hottest JavaScript project is $new_hotness"
-#         echo "$message"
-#         say "$message"
-#         old_hotness="$new_hotness"
-#     fi
-#     sleep 1
-# done
+while true; do
+    new_hotness="$(hottest JavaScript)"
+    if [[ $old_hotness != $new_hotness ]]; then
+        message="Alert! The hottest JavaScript project is $new_hotness"
+        echo "$message"
+        say "$message"
+        old_hotness="$new_hotness"
+    fi
+    sleep 1
+done
